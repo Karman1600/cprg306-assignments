6dd09b205b7b70d4ba405f5b7a2e3cf4423ef0e7
Karman1600 <karmanpartapsingh.sidhu@edu.sait.ca>
2024-05-24 10:44:36 -0600

    week 2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const StudentInfo = () =>  {
+    return (
+        <div>
+            <p>Karmanpartap Singh Sidhu</p>
+            <a href="https://github.com/Karman1600/cprg306-assignments">https://github.com/Karman1600/cprg306-assignments</a>
+        </div>
+    )
+}
+export default StudentInfo;
