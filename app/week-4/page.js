8186a7e1d9b8bafef2363d3f22640ac20eac3314
Karman1600 <karmanpartapsingh.sidhu@edu.sait.ca>
2024-06-16 16:25:01 -0600

    Week-4 submission

---
+++ app/week-4/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NewItem from './new-item'
+
+function page() {
+  return (
+    <main className='justify-center items-center mt-48'>
+        <h2 className='flex justify-center text-xl font-bold text-gray-50'>Week 4  Interactivity</h2>
+        <NewItem />
+    </main>
+  )
+}
+
+export default page
